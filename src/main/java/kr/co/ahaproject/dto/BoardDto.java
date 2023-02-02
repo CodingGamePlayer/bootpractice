95cd6a923b79e665f410095f7e0cd0a3d63cc8d4
kohyuny <hahahahaha@hahah.a>
2023-02-02 10:00:09 +0900

    commit/

---
+++ src/main/java/kr/co/ahaproject/dto/BoardDto.java
@@ -0,0 +1,22 @@
+package kr.co.ahaproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BoardDto {
+
+    private int bno;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDate date;
+ss
+}
